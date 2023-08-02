f2711ceceb9470081d77a9b606b79a0a4ec0298a
Sri-Shanmukha-Chokkapu <srishanmukhachokkapu2022@gmail.com>
2023-08-02 16:16:43 +0530

    third commit

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -180,7 +180,7 @@ class SideBar extends Component {
                   <div className="profile">
                     <img
                       className="sidebar-profile"
-                      src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+                      src="https://res.cloudinary.com/dpkxg8atl/image/upload/v1690961677/Avatar_fckcmw.png"
                       alt="profile-icon"
                     />
                     <div>
